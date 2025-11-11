e477724171d01efd1d0c48b924bcdbd1d760f1b0
Brady Prigge <bradyep@gmail.com>
2025-11-10 18:04:49 -0600

    fix: solution is a class now for test purposes

--- solution.ts
+++ solution.ts
@@ -1,53 +1,67 @@
 import { Fizzbuzz } from "./solutions/fizzbuzz.js";
 
-const nodeParamsToOmit = 2;
-const args: string[] = process.argv.slice(nodeParamsToOmit); // remove node and script paths
+export class Solution {
+    constructor(private processArgs: string[]) {
+        const nodeParamsToOmit = 2;
+        this.args = processArgs.slice(nodeParamsToOmit); // remove node and script paths
+    }
 
-if (args.length < 1) {
-    console.log('Must supply which solution to run');
-    process.exit(0);
-}
+    private args: string[];
 
-args.forEach((arg, index) => {
-    console.log(`Argument ${index + 1}: ${arg}`)
-});
+    run() {
+        if (this.args.length < 1) {
+            console.log('Must supply a solution to run');
+            process.exit(0);
+        }
 
-const solution: string = args[0] ?? '';
+        this.args.forEach((arg, index) => {
+            console.log(`Argument ${index + 1}: ${arg}`)
+        });
 
-switch (solution) {
-    case "fizzbuzz": 
-        console.log("loading fizzbuzz solution");
-        const countToParamValue = getParamValueFor('count-to');
-        const fizzbuzz = new Fizzbuzz(+countToParamValue);
-        fizzbuzz.solve();
-        break;
-    case "fibonacci": 
-        console.log("loading fibonacci solution");
-        break;
-    case "anagram": 
-        console.log("loading anagram solution");
-        break;
-    default:
-        console.log(`solution ${solution} not found`);
-}
+        const solution: string = this.args[0] ?? '';
 
-function getParamValueFor(paramName: string): number | string {
-    const paramNameIndex = args.indexOf('--' + paramName)
-    if (paramNameIndex < 0) { 
-        console.log(`Can't find parameter name: ${paramName}`);
-    } else {
-        console.log(`Found ${paramName} | args.length: ${args.length} | paramNameIndex: ${paramNameIndex}`);
-        if (args.length - 1 <= paramNameIndex) { 
-            console.log(`Found param name at ${paramNameIndex} but size of array is ${args.length}, which is too small, so can't get value`);
-        } else {
-            const paramValue = args[paramNameIndex + 1];
-            if (paramValue) {
-                return paramValue;
-            } else {
-                console.log(`Param value at position ${paramNameIndex + 1} is undefined`)
-            }
+        switch (solution) {
+            case "fizzbuzz":
+                console.log("loading fizzbuzz solution");
+                const countToParamValue = this.getParamValueFor('count-to');
+                if (countToParamValue) {
+                    const fizzbuzz = new Fizzbuzz(+countToParamValue);
+                    fizzbuzz.solve();
+                } else { console.log(`Couldn't get parameters needed for ${solution}`) }
+                break;
+            case "fibonacci":
+                console.log("loading fibonacci solution");
+                break;
+            case "anagram":
+                console.log("loading anagram solution");
+                break;
+            default:
+                console.log(`solution ${solution} not found`);
         }
     }
 
-    return 0;
+    getParamValueFor(paramName: string): number | string | undefined {
+        const paramNameIndex = this.args.indexOf('--' + paramName)
+        if (paramNameIndex < 0) {
+            console.log(`Can't find parameter name: ${paramName}`);
+        } else {
+            console.log(`Found ${paramName} | args.length: ${this.args.length} | paramNameIndex: ${paramNameIndex}`);
+            if (this.args.length - 1 <= paramNameIndex) {
+                console.log(`Found param name at ${paramNameIndex} but size of array is ${this.args.length}, which is too small, so can't get value`);
+            } else {
+                const paramValue = this.args[paramNameIndex + 1];
+                if (paramValue) {
+                    return paramValue;
+                } else {
+                    console.log(`Param value at position ${paramNameIndex + 1} is undefined`)
+                }
+            }
+        }
+
+        return 0;
+    }
+
 }
+
+const solution = new Solution(process.argv);
+solution.run();
