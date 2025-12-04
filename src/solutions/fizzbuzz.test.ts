d9c3047398dd411a5615bcbd7c3299cfc8ba9c3c
Brady Prigge <bradyep@gmail.com>
2025-12-03 19:22:15 -0600

    feat: fizzbuzz tests

--- src/solutions/fizzbuzz.test.ts
+++ src/solutions/fizzbuzz.test.ts
@@ -4,10 +4,31 @@ import type { SolutionParams } from "./SolutionParams"
 import { SolutionType } from './SolutionType';
 
 describe('initial implementation', () => {
-  it('when counting to 16 it gives us 7 lines of output with all the expected fizzes and buzzes in order', () => {
+  it('returns 7 lines of output with all the expected fizzes and buzzes in order when counting to 16', () => {
     const params: SolutionParams = new Map([['count-to', 16]]);
     const fb = new Fizzbuzz(params);
     const result = fb.solve(SolutionType.initial);
     expect(result).toStrictEqual(['fizz', 'buzz', 'fizz', 'fizz', 'buzz', 'fizz', 'fizzbuzz']);
+  })
+
+  it('returns the same results for 15 (no off by one errors)', () => {
+    const params: SolutionParams = new Map([['count-to', 15]]);
+    const fb = new Fizzbuzz(params);
+    const result = fb.solve(SolutionType.initial);
+    expect(result).toStrictEqual(['fizz', 'buzz', 'fizz', 'fizz', 'buzz', 'fizz', 'fizzbuzz']);
+  })
+
+  it('returns just fizz for 3 (no off by one errors)', () => {
+    const params: SolutionParams = new Map([['count-to', 3]]);
+    const fb = new Fizzbuzz(params);
+    const result = fb.solve(SolutionType.initial);
+    expect(result).toStrictEqual(['fizz']);
+  })
+
+  it('returns an empty array for 2 (no off by one errors)', () => {
+    const params: SolutionParams = new Map([['count-to', 2]]);
+    const fb = new Fizzbuzz(params);
+    const result = fb.solve(SolutionType.initial);
+    expect(result).toStrictEqual([]);
   })
 });
